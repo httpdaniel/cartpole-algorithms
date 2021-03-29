0bd4f252575c455f087b5a3555da925b147fff9c
httpdaniel <daniel.097@hotmail.com>
2021-03-29 19:03:48 +0100

    Adding baseline

---
+++ algorithms/Baseline.py
@@ -0,0 +1,92 @@
+# Import packages
+import gym
+import random
+import time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+
+class CartpoleAgent:
+    def __init__(self, env):
+        self.env = env
+        self.action_space = self.env.action_space.n
+
+    # Select random action from action space
+    def update_action(self):
+        action = random.randint(0, self.action_space - 1)
+        return action
+
+
+def main():
+    env = gym.make('CartPole-v1')
+    agent = CartpoleAgent(env)
+
+    final = []
+    start_time = time.time()
+    for ep in tqdm(range(200)):
+        state = env.reset()
+        done, total_reward = False, 0
+        while not done:
+            if ep % 10 == 0:
+                env.render()
+            action = agent.update_action()
+            state, reward, done, _ = env.step(action)
+            total_reward += reward
+        env.close()
+        print('Episode{} Reward={}'.format(ep, total_reward))
+        final.append(total_reward)
+        plot_results(final)
+        if ep == 199:
+            end_time = time.time()
+            time_taken = end_time - start_time
+            get_results(total_reward, ep, time_taken)
+            break
+
+
+# Plot results for each iteration
+def plot_results(values):
+
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
+    fig.suptitle("Random search baseline")
+    ax[0].plot(values, label='score per run')
+    ax[0].axhline(195, c='red', ls='--', label='goal')
+    ax[0].set_xlabel('Episodes')
+    ax[0].set_ylabel('Reward')
+    x = range(len(values))
+    ax[0].legend()
+
+    # Calculate the trend
+    try:
+        z = np.polyfit(x, values, 1)
+        p = np.poly1d(z)
+        ax[0].plot(x, p(x), "--", label='trend')
+    except:
+        print('')
+
+    # Plot the histogram of results
+    ax[1].hist(values[-50:])
+    ax[1].axvline(195, c='red', label='goal')
+    ax[1].set_xlabel('Scores for last 50 Episodes')
+    ax[1].set_ylabel('Frequency')
+    ax[1].legend()
+    plt.show()
+
+
+# Export results to csv
+def get_results(total_reward, ep, time_taken):
+    minutes = time_taken/60
+    minutes = '%.2f' % minutes
+
+    res = {'Final Reward': total_reward,
+           'Number of episodes': ep,
+           'Time Taken (Minutes)': minutes
+           }
+
+    res_df = pd.DataFrame([res], columns=['Final Reward', 'Number of episodes', 'Time Taken (Minutes)'])
+    res_df.to_csv('../results/baseline-results.csv', index=False, encoding='utf-8')
+
+
+if __name__ == "__main__":
+    main()
