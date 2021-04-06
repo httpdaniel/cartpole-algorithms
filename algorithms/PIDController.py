9049252f9a417b6276394f921b90bb7c5e6fa0fd
clairefarrell <32479890+clairefarrell@users.noreply.github.com>
2021-04-06 18:16:00 +0100

    added random decision making for the agent with a sigmoid function

--- algorithms/PIDController.py
+++ algorithms/PIDController.py
@@ -1,14 +1,19 @@
 import gym
+import numpy as np
+import random
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 cartpole_env = gym.make('CartPole-v1')
 cartpole_env._max_episode_steps = 10000
 cartpole_env.seed(7)
 current_state = cartpole_env.reset()
-action = 1
 done = False
 total_reward = 0
 while not done:
-    current_state, reward, done, debug_info = cartpole_env.step(action)
+    action = np.round(sigmoid(random.uniform(-1,1)))
+    current_state, reward, done, debug_info = cartpole_env.step(action.astype(np.int32))
     total_reward += reward
 print(total_reward)
 cartpole_env.close()
