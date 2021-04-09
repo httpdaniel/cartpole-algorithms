ca6e693bc2df7e4ace30a9365939e93c7e1c4f7e
joshuacassidy <joshcassidy79@gmail.com>
2021-04-09 11:40:23 +0100

    implemented gradient descent to optimise pid controller parameters

--- algorithms/PIDController.py
+++ algorithms/PIDController.py
@@ -1,9 +1,49 @@
 import gym
 import numpy as np
 import random
+import time
+random.seed(7)
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+def gradient_descent(learning_rate = 0.05, epochs = 1000):
+    best_pid = None
+    total_reward = 0
+    av_reward = 0
+    episode = 0
+    time_taken = 0
+    final = 0
+    prev_reward = None
+   
+    P, I, D = random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)
+    for i in range(epochs):
+        start_time = time.time()
+        reward = pid(P, I, D)
+
+        P = P - (learning_rate * reward["error"][0])
+        I = I - (learning_rate * reward["error"][1])
+        D = D - (learning_rate * reward["error"][2])
+        end_time = time.time()
+        
+        if not best_pid or best_pid["reward"] < reward["total_reward"]:
+            time_taken = end_time - start_time
+            best_pid = {
+                "P": P,
+                "I": I,
+                "D": D,
+                "reward": reward["total_reward"],
+                "time_taken": time_taken
+            }
+            total_reward = reward["total_reward"]
+            av_reward = reward["av_reward"]
+            episode = reward["episode"]
+            final = reward["final"]
+        prev_reward = reward
+        if reward["total_reward"] >=  1000:
+            break
+    print(best_pid)        
+          
 
 
 def pid(P, I, D): 
@@ -14,6 +54,8 @@ def pid(P, I, D):
     done = False
     total_reward = 0
     final = []
+    reward_count = 0
+
     goal_state = np.array([0, 0, 0, 0])
     for episode in range(20):
         total_reward = 0
@@ -63,6 +105,4 @@ def pid(P, I, D):
     }
     
 
-
-
-pid(P = 1, I = 0.1, D = 0.5)
+gradient_descent()
